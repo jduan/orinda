e2180d235fe57e718375fda4fbef3d746b8ab670
Jingjing Duan <duanjingjing@gmail.com>
2018-09-26 22:25:20 -0700

    read file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import Lib
+import System.Environment
+import System.IO
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  myFile <- openFile (head args) ReadMode
+  hClose myFile
+  putStrLn "done!"
