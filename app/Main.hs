199800440eb416095b0513c05ed6b9e853f6ceb7
Jingjing Duan <duanjingjing@gmail.com>
2018-09-27 22:10:17 -0700

    count words

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,16 @@
 module Main where
 
-import Lib
 import System.Environment
-import System.IO
+
+countData :: String -> (Int, Int, Int)
+countData s = (length s, length (words s), length (lines s))
+
+countStrFmt :: (Int, Int, Int) -> String
+countStrFmt (chars, words, lines) =
+  unwords ["chars:", show chars, "words:", show words, "lines:", show lines]
 
 main :: IO ()
 main = do
   args <- getArgs
-  myFile <- openFile (head args) ReadMode
-  hClose myFile
-  putStrLn "done!"
+  fileData <- readFile (head args)
+  putStrLn $ (countStrFmt . countData) fileData
